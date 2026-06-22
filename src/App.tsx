d8aff35b033d64558c3252ec2259a7486207b385
subashaathi <127607801+subashaathi@users.noreply.github.com>
2026-06-22 17:51:51 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import Login from "./pages/Login";
+import AdminDashboard from "./pages/AdminDashboard";
+import UserDashboard from "./pages/UserDashboard";
+
+function App() {
+  return (
+    <BrowserRouter>
+      {/* ✅ Global Toast Container */}
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+        draggable
+        theme="light"
+      />
+
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        <Route path="/user/dashboard" element={<UserDashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
